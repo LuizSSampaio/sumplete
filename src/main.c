d1d5aff8aea7f61d05dc3a008ffd0b908909496b
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-02-06 12:34:27 -0300

    feat: make help command bold

--- src/main.c
+++ src/main.c
@@ -18,17 +18,18 @@ int main() {
 
 void helpCommand() {
   printf("Comandos do jogo\n");
-  printf("ajuda: Exibe os comandos do jogo\n");
-  printf("sair: Sair do Jogo\n");
-  printf("novo: Começar um novo jogo\n");
-  printf("carregar: Carregar um jogo salvo em arquivo\n");
-  printf("ranking: Exibir o ranking\n");
-  printf("salvar: Salva o jogo atual\n");
-  printf("dica: Marca uma posição no jogo\n");
-  printf("resolver: Resolve o jogo atual\n");
-  printf("adicionar <lin> <col>: marca a posição <lin> <col> para entrar na "
+  printf("\e[1majuda:\e[m Exibe os comandos do jogo\n");
+  printf("\e[1msair:\e[m Sair do Jogo\n");
+  printf("\e[1mnovo:\e[m Começar um novo jogo\n");
+  printf("\e[1mcarregar:\e[m Carregar um jogo salvo em arquivo\n");
+  printf("\e[1mranking:\e[m Exibir o ranking\n");
+  printf("\e[1msalvar:\e[m Salva o jogo atual\n");
+  printf("\e[1mdica:\e[m Marca uma posição no jogo\n");
+  printf("\e[1mresolver:\e[m Resolve o jogo atual\n");
+  printf("\e[1madicionar <lin> <col>:\e[m marca a posição <lin> <col> para "
+         "entrar na "
          "soma\n");
-  printf("remover <lin> <col>: remove a posição da soma\n");
+  printf("\e[1mremover <lin> <col>:\e[m remove a posição da soma\n");
 }
 
 void repl() {
